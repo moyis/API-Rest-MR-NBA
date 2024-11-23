f2fcfe0fc5535df4eaf780c8427bacaa45b1c00c
Fausto Moya <faustomoya99@gmail.com>
2024-11-23 01:03:19 +0100

    fix: use PropertyNamingStrategies.SnakeCaseStrategy in DTO

--- src/main/java/com/apinba/restapi/controllers/model/GameDto.java
+++ src/main/java/com/apinba/restapi/controllers/model/GameDto.java
@@ -1,24 +1,36 @@
 package com.apinba.restapi.controllers.model;
 
 import com.apinba.restapi.models.GameModel;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import java.util.UUID;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record GameDto(
-        UUID id,
-        String date,
-        int season,
-        String status,
-        int period,
-        String time,
-        boolean postseason,
-        int home_team_score,
-        int visitor_team_score,
-        TeamDto local_team,
-        TeamDto visitor_team
-)
-{
+    UUID id,
+    String date,
+    int season,
+    String status,
+    int period,
+    String time,
+    boolean postseason,
+    int homeTeamScore,
+    int visitorTeamScore,
+    TeamDto localTeam,
+    TeamDto visitorTeam) {
 
-    public static GameDto from(GameModel game){
-        return new GameDto(game.getId(), game.getDate(), game.getSeason(), game.getStatus(), game.getPeriod(), game.getTime(), game.isPostseason(), game.getHome_team_score(), game.getVisitor_team_score(), TeamDto.from(game.getLocal_team()), TeamDto.from(game.getVisitor_team()));
-    }
+  public static GameDto from(GameModel game) {
+    return new GameDto(
+        game.getId(),
+        game.getDate(),
+        game.getSeason(),
+        game.getStatus(),
+        game.getPeriod(),
+        game.getTime(),
+        game.isPostseason(),
+        game.getHome_team_score(),
+        game.getVisitor_team_score(),
+        TeamDto.from(game.getLocal_team()),
+        TeamDto.from(game.getVisitor_team()));
+  }
 }
--- src/main/java/com/apinba/restapi/controllers/model/TeamDto.java
+++ src/main/java/com/apinba/restapi/controllers/model/TeamDto.java
@@ -1,26 +1,28 @@
 package com.apinba.restapi.controllers.model;
 
 import com.apinba.restapi.models.TeamModel;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import java.util.UUID;
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public record TeamDto(
+    UUID id,
+    String abbreviation,
+    String city,
+    String conference,
+    String division,
+    String fullName,
+    String name) {
 
-        UUID id,
-        String abbreviation,
-
-        String city,
-
-        String conference,
-
-        String division,
-
-        String full_name,
-
-        String name
-
-        ) {
-
-    public static TeamDto from(TeamModel team){
-        return new TeamDto(team.getId(), team.getAbbreviation(), team.getCity(), team.getConference(), team.getDivision(), team.getFull_name(), team.getName());
-    }
+  public static TeamDto from(TeamModel team) {
+    return new TeamDto(
+        team.getId(),
+        team.getAbbreviation(),
+        team.getCity(),
+        team.getConference(),
+        team.getDivision(),
+        team.getFull_name(),
+        team.getName());
+  }
 }
