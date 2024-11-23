c8990b3509e211c5f1e5255435d3711366dc51aa
Fausto Moya <faustomoya99@gmail.com>
2024-11-23 01:03:43 +0100

    fix: remove @SpringBootApplication from RestapiTestApplication

--- src/test/java/com/apinba/restapi/RestapiTestApplication.java
+++ src/test/java/com/apinba/restapi/RestapiTestApplication.java
@@ -1,15 +1,12 @@
 package com.apinba.restapi;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class RestapiTestApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.from(RestapiApplication::main)
-                .with(TestcontainersConfiguration.class)
-                .run(args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.from(RestapiApplication::main)
+        .with(TestcontainersConfiguration.class)
+        .run(args);
+  }
 }
